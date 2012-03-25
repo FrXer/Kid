37a11c618a00b2bcd55fa16a93d2afa97702ee1e
Frank <frank.xia.cn@gmail.com>
2012-03-25 13:35:30 +0800

    first commit

---
+++ Kid4Math/src/com/kid/math/Kid4MathActivity.java
@@ -0,0 +1,13 @@
+package com.kid.math;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Kid4MathActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
