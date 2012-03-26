3b6b0d9328e5eba1d0eb83241795e677e2b904b2
FrX <frank.xia.cn@gmail.com>
2012-03-26 18:14:00 +0800

    3rd

--- Kid4Math/src/com/kid/math/Kid4MathActivity.java
+++ Kid4Math/src/com/kid/math/Kid4MathActivity.java
@@ -1,13 +1,128 @@
 package com.kid.math;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Random;
 
-public class Kid4MathActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Kid4MathActivity extends SimpleBaseGameActivity {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	private BitmapTextureAtlas mBitmapTextureAtlas;
+	private ITextureRegion mFaceTextureRegion;
+	private Font mFontQuestion;
+	private Text text;
+	Random r = new Random(20);
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		Toast.makeText(this, "Touch & Drag the face!", Toast.LENGTH_LONG).show();
+
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+	}
+
+	@Override
+	public void onCreateResources() {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+
+		this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 32, 32, TextureOptions.BILINEAR);
+		this.mFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapTextureAtlas, this, "face_box.png", 0, 0);
+		this.mBitmapTextureAtlas.load();
+
+		// debug
+		// Load the font
+		FontFactory.setAssetBasePath("font/");
+		this.mFontQuestion = FontFactory.createFromAsset(this.getFontManager(), this.getTextureManager(), 512, 512, TextureOptions.BILINEAR, this.getAssets(), "EraserDust.ttf", 30, true, Color.WHITE);
+		this.mFontQuestion.load();
+	}
+
+	@Override
+	public Scene onCreateScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene();
+		scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+
+		// debug
+		text = new Text(0, 0, mFontQuestion, "Test", 50, this.getVertexBufferObjectManager());
+		scene.attachChild(text);
+		//
+		final float centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
+		final float centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
+		final Sprite face = new Sprite(centerX, centerY, this.mFaceTextureRegion, this.getVertexBufferObjectManager()) {
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
+
+				showMessage();
+				// Kid4MathActivity.this.text.setText(r1 + "+" + r2 + " = " + r1 * r2);
+				return true;
+			}
+
+		};
+		face.setScale(4);
+		scene.attachChild(face);
+		scene.registerTouchArea(face);
+		// scene.setTouchAreaBindingOnActionDownEnabled(true);
+
+		return scene;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	private void showMessage() {
+		// debug
+
+		int r1 = r.nextInt(20);
+		int r2 = r.nextInt(20);
+		// bug ?
+		Log.d("Log", r1 + "+" + r2 + " = " + r1 * r2);
+		text.setText(r1 + "+" + r2 + " = " + r1 * r2);
+	}
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
 }
